0fc6234240a5b66c2a8c04128bc368de50a3c598
Philip James <pjj@philipjohnjames.com>
2017-06-11 13:48:07 -0700

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='epithet',
-    version='0.0.2',
+    version='0.0.3',
     description="Manage your labels.",
     long_description=readme + '\n\n' + history,
     author="Philip James",
