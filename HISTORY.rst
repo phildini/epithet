de398d819cfde58d4ccc59169bc7ee8e4917ecca
Philip James <pjj@philipjohnjames.com>
2017-06-11 13:47:40 -0700

    Add delete command, allow GHE urls

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,6 +2,13 @@
 History
 =======
 
+0.0.3 (2017-06-11)
+------------------
+
+* Add "delete" command
+* Allow specifying an API URL for GitHub Enterprise
+* Make `--key` param required
+
 0.0.2 (2017-05-25)
 ------------------
 
--- epithet/epithet.py
+++ epithet/epithet.py
@@ -8,8 +8,11 @@ from github.GithubException import RateLimitExceededException
 def main():
     cli(obj={})
 
-def get_repos(key, org, repo):
-    g = Github(key)
+def get_repos(key, org, repo, url):
+    if url:
+        g = Github(key, base_url=url)
+    else:
+        g = Github(key)
     if org:
         g_org = g.get_organization(login=org)
     else:
@@ -24,9 +27,11 @@ def get_repos(key, org, repo):
 
 @click.group()
 @click.option('--dryrun', is_flag=True, help="Don't actually change or create labels")
+@click.option('--url', help="API URL - change if GitHub Enterprise")
 @click.pass_context
-def cli(ctx, dryrun):
+def cli(ctx, dryrun, url):
     ctx.obj['dryrun'] = dryrun
+    ctx.obj['url'] = url
 
 
 @cli.command()
@@ -35,13 +40,14 @@ def cli(ctx, dryrun):
 @click.option('--repo', help="Optionally select a single repo")
 @click.pass_context
 def list(ctx, key, org, repo):
-    try:
-        for repo in get_repos(key, org, repo):
-            click.echo("\n * {}:\n".format(repo.name))
-            for label in repo.get_labels():
-                click.echo(" - {} ({})".format(label.name, label.color))
-    except RateLimitExceededException:
-        click.echo("GitHub rate limit exceeded: Try again with a --key passed in")
+    if not key:
+        click.echo("You must provide a GitHub API v3 key")
+        return
+    for repo in get_repos(key, org, repo, ctx.obj['url']):
+        click.echo("\n * {}:\n".format(repo.name))
+        for label in repo.get_labels():
+            click.echo(" - {} ({})".format(label.name, label.color))
+
 
 
 @cli.command()
@@ -52,31 +58,56 @@ def list(ctx, key, org, repo):
 @click.option('--repo', help="Optionally select a single repo")
 @click.pass_context
 def add(ctx, name, color, key, org, repo):
+    if not key:
+        click.echo("You must provide a GitHub API v3 key")
+        return
     click.echo("Adding a label with name: {} and  color: {}".format(name, color))
-    try:
-        for repo in get_repos(key, org, repo):
-            click.echo(" * Checking {}".format(repo.name))
-            labels = {}
-            for label in repo.get_labels():
-                labels[label.name] = label
-            if name in labels:
-                click.echo(
-                    " - Found {} on {} (Dryrun: {})".format(
-                        labels[name].name, repo.name, ctx.obj['dryrun']
-                    )
+    for repo in get_repos(key, org, repo, ctx.obj['url']):
+        click.echo(" * Checking {}".format(repo.name))
+        labels = {}
+        for label in repo.get_labels():
+            labels[label.name] = label
+        if name in labels:
+            click.echo(
+                " - Found {} on {} (Dryrun: {})".format(
+                    labels[name].name, repo.name, ctx.obj['dryrun']
                 )
-                if labels[name].color != color and not ctx.obj['dryrun']:
-                    labels[name].edit(name=name, color=color)
-            else:
-                click.echo(
-                    " - Creating {} on {} (Dryrun: {})".format(
-                        name, repo.name, ctx.obj['dryrun']
-                    )
+            )
+            if labels[name].color != color and not ctx.obj['dryrun']:
+                labels[name].edit(name=name, color=color)
+        else:
+            click.echo(
+                " - Creating {} on {} (Dryrun: {})".format(
+                    name, repo.name, ctx.obj['dryrun']
                 )
-                if not ctx.obj['dryrun']:
-                    repo.create_label(name=name, color=color)
-    except RateLimitExceededException:
-        click.echo("GitHub rate limit exceeded: Try again with a --key passed in")
+            )
+            if not ctx.obj['dryrun']:
+                repo.create_label(name=name, color=color)
+
+@cli.command()
+@click.option('--name', help="Name of label to delete")
+@click.option('--key', help="OAuth Token")
+@click.option('--org', help="Organization to get repos from")
+@click.option('--repo', help="Optionally select a single repo")
+@click.pass_context
+def delete(ctx, name, key, org, repo):
+    if not key:
+        click.echo("You must provide a GitHub API v3 key")
+        return
+    click.echo("Deleting label: {}".format(name))
+    for repo in get_repos(key, org, repo, ctx.obj['url']):
+        click.echo(" * Checking {}".format(repo.name))
+        labels = {}
+        for label in repo.get_labels():
+            labels[label.name] = label
+        if name in labels:
+            click.echo(
+                " - Found {} on {}, deleting (Dryrun: {})".format(
+                    labels[name].name, repo.name, ctx.obj['dryrun']
+                )
+            )
+            if not ctx.obj['dryrun']:
+                labels[name].delete()
 
 
 if __name__ == "__main__":
